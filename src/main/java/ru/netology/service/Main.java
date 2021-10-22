d6deef3640a631a185c81e6faa42f50272db99fe
bairb <bairu@yandex.ru>
2021-10-22 13:05:24 +0300

    asdad

---
+++ src/main/java/ru/netology/service/Main.java
@@ -0,0 +1,17 @@
+package ru.netology.service;
+
+public class Main {
+    public static void main(String[] args) {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        {
+            System.out.println("It should return 100 if less 1000");
+            int result = cashbackHackService.remain(900);
+            System.out.println(100 == result);
+        }
+        {
+            System.out.println("It should return 900 if more 1000");
+            int result = cashbackHackService.remain(1100);
+            System.out.println(900 == result);
+        }
+    }
+}
