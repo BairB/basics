cd7be7c7d7c7cf9b8b4833e5a95e858d1eb37008
bairb <bairu@yandex.ru>
2021-10-22 12:52:08 +0300

    tests added

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,33 @@
+package ru.netology.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CashbackHackServiceTest {
+    @Test
+     public void shouldReturn100ifLessThan1000(){
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int actual = cashbackHackService.remain(900);
+        int expected = 100;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldReturn0if1000(){
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int actual = cashbackHackService.remain(1000);
+        int expected = 0;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldReturn900ifMoreThan1000(){
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int actual = cashbackHackService.remain(1100);
+        int expected = 900;
+        assertEquals(expected, actual);
+    }
+
+
+}
